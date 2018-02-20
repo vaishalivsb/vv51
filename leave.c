7b50599dda85542f02b4b6a60511a883913bb502
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2018-02-20 10:35:10 +0000

    Create leave.c

---
+++ leave.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int i;
+	char a[10];
+	scanf("%s",&a);
+	for(i=4;i>=0;i--)
+	{
+		if((a[i]=='a')||(a[i]=='e')||(a[i]=='i')||(a[i]=='o')||(a[i]=='u'))
+	{
+		
+	}
+	else
+	{
+		printf("%c",a[i]);
+	}
+		
+	}
+
+return 0;
+}
